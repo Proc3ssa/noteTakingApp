ac81a8a26cd82f5fcc51e05636c424319d1618a8
Faisal <pros3sa@gmail.com>
2025-06-22 20:55:46 +0000

    chore: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { SplashscreenComponent } from './pages/splashscreen/splashscreen.component';
+import { WalkthroughComponent } from './pages/walkthrough/walkthrough.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CreateComponent } from './pages/create/create.component';
+import { NoteComponent } from './pages/note/note.component';
+import { NotesComponent } from './pages/notes/notes.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ArchivedComponent } from './pages/archived/archived.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: SplashscreenComponent},
+    {path: '/walkthrough', component: WalkthroughComponent},
+    {path: '/signup', component: SignupComponent},
+    {path: '/login', component :LoginComponent},
+    {path: '/create', component:CreateComponent},
+    {path: '/notes/:id', component:NoteComponent},
+    {path: '/notes/:id', component:NoteComponent},
+    {path: '/notes', component:NotesComponent},
+    {path: '/home', component:HomeComponent},
+    {path: '/archived', component:ArchivedComponent},
+
+
+
+
+
+];
