d57e70687bc6d3ff900eb07c8b52885f189406e9
Faisal <pros3sa@gmail.com>
2025-06-22 20:42:41 +0000

    feat: add pages

---
+++ src/app/pages/splashscreen/splashscreen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splashscreen',
+  imports: [],
+  templateUrl: './splashscreen.component.html',
+  styleUrl: './splashscreen.component.scss'
+})
+export class SplashscreenComponent {
+
+}
---
+++ src/app/pages/walkthrough/walkthrough.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-walkthrough',
+  imports: [],
+  templateUrl: './walkthrough.component.html',
+  styleUrl: './walkthrough.component.scss'
+})
+export class WalkthroughComponent {
+
+}
