3f626f01269d01557f15d88245678eb84c7dcc79
Faisal <pros3sa@gmail.com>
2025-06-23 17:11:31 +0000

    feat: add home screen

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
-
+import { SpaceComponent } from '../../components/space/space.component';
+import { ɵnoSideEffects } from '@angular/core';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { FoldersComponent } from '../../components/folders/folders.component';
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  standalone: true,
+  imports: [HeaderComponent, FoldersComponent, SpaceComponent, SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
