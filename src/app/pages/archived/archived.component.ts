c1b13db720cbe8ba54868f97a2cabdc0ed944216
Faisal <pros3sa@gmail.com>
2025-06-25 10:47:27 +0000

    feat: add archives page

--- src/app/pages/archived/archived.component.ts
+++ src/app/pages/archived/archived.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
-
+import { CrudServiceService } from '../../services/crud-service.service';
+import { Note } from '../../models/note';
+import { NotecardComponent } from '../../components/notecard/notecard.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-archived',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, NotecardComponent, CommonModule],
   templateUrl: './archived.component.html',
   styleUrl: './archived.component.scss'
 })
-export class ArchivedComponent {
+export class ArchivedComponent implements OnInit {
+
+  notes: Note[] = [];
+
+  constructor(private crudService: CrudServiceService) {}
+
+  ngOnInit(): void {
+    this.getArchivedNotes();
+  }
+
+  getArchivedNotes(): void {
+    this.crudService.getNotes('Archived').subscribe(notes => {
+      console.log('Archived notes:', notes);
+      this.notes = notes;
+    });
+  }
 
   openModalHandler(){
-    
+
   }
 
 }
